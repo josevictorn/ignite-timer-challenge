d933efc497cb7402a1196a0ebc55a43be47a9df7
victor <josevictornascimento2016@gmail.com>
2023-03-26 22:20:43 -0300

    first commit

---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,43 @@
+import { FormContainer, TaksInput, MinutesAmountInput } from './styles'
+import { useContext } from 'react'
+import { useFormContext } from 'react-hook-form'
+import { CyclesContext } from '../../../../contexts/CyclesContext'
+
+export function NewCycleForm() {
+  const { activeCycle } = useContext(CyclesContext)
+  const { register } = useFormContext()
+
+  return (
+    <FormContainer>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <TaksInput
+        id="task"
+        list="task-suggestions"
+        disabled={!!activeCycle}
+        placeholder="Dê um nome para o seu projeto"
+        {...register('task')}
+      />
+
+      <datalist id="task-suggestions">
+        <option value="Projeto 1" />
+        <option value="Projeto 2" />
+        <option value="Projeto 3" />
+        <option value="Banana" />
+      </datalist>
+
+      <label htmlFor="minutesAmount">durante</label>
+      <MinutesAmountInput
+        type="number"
+        id="minutesAmount"
+        placeholder="00"
+        step={5}
+        min={0}
+        max={60}
+        disabled={!!activeCycle}
+        {...register('minutesAmount', { valueAsNumber: true })}
+      />
+
+      <span>minutos.</span>
+    </FormContainer>
+  )
+}
